f1b2d3d6966a2dfcbfca5a6c1deed2343814a123
Phani <phani.vadluri@gmail.com>
2019-11-16 19:27:54 +0530

    Added HomeController

--- src/main/java/GitCommit/HelloController.java
+++ src/main/java/GitCommit/HelloController.java
@@ -1,5 +1,17 @@
 package GitCommit;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+	@GetMapping("/cult")
+	public String getMap()
+	{
+		return "I will come back";
+	}
 
 }
